9428ba3e6f99274c460fe0bb7e68f6bb34cc2fc7
codexakshay <codexakshay@gmail.com>
2020-09-05 14:45:52 +0530

    UC_flipCoinSimulator

--- UC_flipCoinSimulator.sh
+++ UC_flipCoinSimulator.sh
@@ -1,6 +1,16 @@
+h_c=0
+l_c=0
+for ((i=1;i<10;i++))
+do
 flip=$(($(($RANDOM%10))%2))
 if [ $flip -eq 1 ]; then
     echo "HEADS"
+        h_c=$(($h_c+1))
 else
     echo "TAILS"
+        l_c=$(($l_c+1))
 fi
+done
+echo "HEAD COUNT : " $h_c
+echo "TAIL COUNT : " $l_c
+
